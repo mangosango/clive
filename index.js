8d81fc4c4f8d88e807ce9557d38d643ea558dff4
Jared Kotoff <jaredkotoff@users.noreply.github.com>
2017-12-23 01:26:58 -0800

    Twitch Clips GET API Features (#2)
    * Update .gitignore to ignore .idea folder from intellij
    
    * Use es6 let and const for variables
    
    * Clean up twitch channels initialization
    
    * Add mod and subs only flags that can be turned on to limit to those who have those permissions
    
    * Setup prettier, eslint, and autoformatting via husky
    
    * Run prettier on js files
    
    * Install dotenv to read from .env files
    
    * Read from .env files for non-production server and add an example env file
    
    * Add some debug logging output
    
    * Add some comments, fix backward error log in post response, use === for booleans, switch anonymous functions to es6 notation
    
    * Parse the URL slug from posted twitch clips to be used in API calls
    
    * Enhanced data if a Twitch Client ID is provided. Resolve the name of the person who clipped, the title, and the game. Also broadcaster only flag
    
    * Update readme and remove setting constants in index.js. Everything should be set as an environment variable (prod) or .env file (dev/debugging)
    
    * Update README.md
    
    * Only clips from restricted channels will be shared by default
    
    * Run prettier on markdown
    
    * Update readme since clive only sends clips from twitch channel list

--- index.js
+++ index.js
@@ -1,72 +1,244 @@
-const _ = require('lodash');
-
-const DISCORD_WEBHOOK_URL = _.get(process, 'env.DISCORD_WEBHOOK_URL') || "YOUR_DISCORD_WEBHOOK_URL_HERE";
-let channelArray = _.get(process, 'env.TWITCH_CHANNELS');
-if (channelArray) {
-	channelArray = _.split(channelArray, ' ');
-} else {
-	channelArray = ['YOUR_TWITCH_CHANNEL_HERE'];
+'use strict';
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
 }
-const TWITCH_CHANNELS = channelArray.map(function (channel) {
-	return `#${channel}`;
-});
+const _ = require('lodash');
+const request = require('request-promise');
+const tmi = require('tmi.js');
+const URI = require('urijs');
+const winston = require('winston');
+winston.level = _.get(process, 'env.LOG_LEVEL') || 'error';
 
-var request = require('request');
-var tmi = require("tmi.js");
+const DISCORD_WEBHOOK_URL = _.get(process, 'env.DISCORD_WEBHOOK_URL');
+const TWITCH_CHANNELS = generateChannelList(
+  _.get(process, 'env.TWITCH_CHANNELS'),
+);
+const TWITCH_CLIENT_ID = _.get(process, 'env.TWITCH_CLIENT_ID') || null;
+const RESTRICT_CHANNELS = _.get(process, 'env.RESTRICT_CHANNELS') || true;
+const BROADCASTER_ONLY =
+  _.get(process, 'env.BROADCASTER_ONLY') === 'true' || false;
+const MODS_ONLY = _.get(process, 'env.MODS_ONLY') === 'true' || false;
+const SUBS_ONLY = _.get(process, 'env.SUBS_ONLY') === 'true' || false;
 
-var options = {
-  options: {
-    debug: false
-  },
-  connection: {
-    reconnect: true
-  },
-  channels: TWITCH_CHANNELS
-};
+let TWITCH_CHANNEL_IDS = [];
+if (TWITCH_CLIENT_ID && RESTRICT_CHANNELS) {
+  resolveTwitchUsernamesToIds(TWITCH_CHANNELS).then(userIds => {
+    TWITCH_CHANNEL_IDS = userIds;
+    logStartInfo();
+  });
+}
 
-var client = new tmi.client(options);
+function logStartInfo() {
+  winston.log('info', 'Config settings:\n', {
+    DISCORD_WEBHOOK_URL,
+    TWITCH_CHANNELS,
+    TWITCH_CHANNEL_IDS,
+    RESTRICT_CHANNELS,
+    BROADCASTER_ONLY,
+    MODS_ONLY,
+    SUBS_ONLY,
+  });
+  winston.log(
+    'info',
+    `Twitch Client ID is ${TWITCH_CLIENT_ID ? '' : 'NOT '}set`,
+  );
 
-client.on("message", function (channel, userstate, message, self) {
-  // Don't listen to my own messages..
-  if (self) return;
+  createTmiClient();
+}
 
-  // Handle different message types..
-  switch(userstate["message-type"]) {
-    case "action":
-      // This is an action message..
-      break;
-    case "chat":
-      // console.log(userstate);
-      if (message.indexOf("clips.twitch.tv/") !== -1) {
-        postThing(`**${userstate["display-name"]}** posted a clip: ${message}`);
-      }
-      break;
-    case "whisper":
-      // This is a whisper..
-      break;
-    default:
-      // Something else ?
-      break;
+function createTmiClient() {
+  const tmiOptions = {
+    options: {
+      debug: _.get(process, 'env.LOG_LEVEL') === 'debug' || false,
+    },
+    connection: {
+      reconnect: true,
+    },
+    channels: TWITCH_CHANNELS,
+  };
+
+  const client = new tmi.client(tmiOptions);
+
+  // Check messages that are posted in twitch chat
+  client.on('message', (channel, userstate, message, self) => {
+    winston.log('debug', 'New message');
+    winston.log('debug', 'Channel: ', channel);
+    winston.log('debug', 'Userstate: ', userstate);
+    winston.log('debug', 'Message: ', message);
+
+    // Don't listen to my own messages..
+    if (self) return;
+    // Broadcaster only mode
+    const isBroadcaster = userstate['badges'].broadcaster === '1';
+    if (BROADCASTER_ONLY && !isBroadcaster) {
+      winston.log('debug', `NON-BROADCASTER posted a clip: ${message}`);
+      return;
+    }
+    // Mods only mode
+    if (MODS_ONLY && !(userstate['mod'] || isBroadcaster)) {
+      winston.log('debug', `NON-MOD posted a clip: ${message}`);
+      return;
+    }
+    // Subs only mode
+    if (SUBS_ONLY && !userstate['subscriber']) {
+      winston.log('debug', `NON-SUB posted a clip: ${message}`);
+      return;
+    }
+
+    // Handle different message types..
+    switch (userstate['message-type']) {
+      case 'action':
+        // This is an action message..
+        break;
+      case 'chat':
+        if (message.indexOf('clips.twitch.tv/') !== -1) {
+          winston.log('debug', `Clip detected in message: ${message}`);
+          if (TWITCH_CLIENT_ID) {
+            postUsingTwitchAPI(message);
+          } else {
+            postUsingMessageInfo(message);
+          }
+        }
+        break;
+      case 'whisper':
+        // This is a whisper..
+        break;
+      default:
+        // Something else ?
+        break;
+    }
+  });
+
+  // Connect the client to the server..
+  client.connect();
+}
+
+function postUsingTwitchAPI(message) {
+  const clipId = getUrlSlug(message);
+  twitchApiGetCall('clips', clipId).then(clipInfo => {
+    winston.log('debug', 'Twitch clip results:', clipInfo);
+
+    if (
+      RESTRICT_CHANNELS &&
+      TWITCH_CHANNEL_IDS.indexOf(clipInfo.broadcaster_id) === -1
+    ) {
+      winston.log('info', 'Clip from unselected channel was posted in chat');
+      return;
+    }
+
+    Promise.all([
+      twitchApiGetCall('users', clipInfo.creator_id),
+      twitchApiGetCall('games', clipInfo.game_id),
+    ]).then(results => {
+      winston.log('debug', 'Async results:', results);
+      postToDiscord(
+        `**${results[0].display_name}** posted a clip during ${
+          results[1].name
+        }: *${clipInfo.title}*\n${clipInfo.url}`,
+      );
+    });
+  });
+}
+
+function postUsingMessageInfo(message) {
+  postToDiscord(`**${userstate['display-name']}** posted a clip: ${message}`);
+}
+
+function getUrlSlug(message) {
+  // split message by spaces, then filter out anything that's not a twitch clip
+  const urls = _.filter(_.split(message, ' '), messagePart => {
+    return messagePart.indexOf('clips.twitch.tv/') !== -1;
+  });
+  winston.log('debug', `Found ${urls.length} urls: `, urls);
+  if (urls.length < 1) {
+    winston.log('error', 'No urls found in message', message);
+    return;
   }
-});
 
-// Connect the client to the server..
-client.connect();
+  const path = URI(urls[0]).path();
+  const clipId = path.replace('/', '');
+  if (!path || !clipId) {
+    winston.log('error', 'Something wrong with this url', urls[0]);
+    return;
+  }
+  winston.log('debug', `Clip slug: ${clipId}`);
+  return clipId;
+}
 
-function postThing(val) {
+async function twitchApiGetCall(endpoint, id) {
+  if (!TWITCH_CLIENT_ID) return;
+  const options = {
+    uri: `https://api.twitch.tv/helix/${endpoint}`,
+    qs: {
+      id: id,
+    },
+    headers: {
+      'Client-ID': TWITCH_CLIENT_ID,
+    },
+    json: true,
+  };
+  winston.log('info', `Calling /${endpoint}?id=${id}`);
+  try {
+    const response = await request(options);
+    return response.data[0];
+  } catch (err) {
+    winston.log('error', `Error calling twitch API /${endpoint}:`, err);
+    return;
+  }
+}
+
+async function resolveTwitchUsernamesToIds(usernames) {
+  if (!TWITCH_CLIENT_ID) return [];
+
+  const usernameFuncs = usernames.map(async username => {
+    const options = {
+      uri: `https://api.twitch.tv/helix/users`,
+      qs: {
+        login: username.replace('#', ''),
+      },
+      headers: {
+        'Client-ID': TWITCH_CLIENT_ID,
+      },
+      json: true,
+    };
+    winston.log('info', `Calling /users?login=${username}`);
+    try {
+      const response = await request(options);
+      request(options);
+      return response.data[0].id;
+    } catch (err) {
+      winston.log('error', `Error calling twitch API /users:`, err);
+      return;
+    }
+  });
+  return await Promise.all(usernameFuncs).then(userIds => userIds);
+}
+
+function postToDiscord(val) {
   request.post(
     DISCORD_WEBHOOK_URL,
-    { json:
-      {
+    {
+      json: {
         content: val,
-        username: "Clive",
-        avatar_url: "http://i.imgur.com/9s3TBNv.png",
+        username: 'Clive',
+        avatar_url: 'http://i.imgur.com/9s3TBNv.png',
+      },
+    },
+    (error, response, body) => {
+      if (error) {
+        winston.log('error', 'Error posting to Discord', response, body);
+      } else if (response.statusCode === 200) {
+        winston.log('info', body);
       }
     },
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body)
-      }
-    }
   );
+}
+
+// Takes space-separated string of twitch channels parses them, adds a # prefix, and puts them into an array
+function generateChannelList(channelsString) {
+  let channelArray = _.split(channelsString, ' ');
+
+  return channelArray.map(channel => {
+    return `#${channel.toLowerCase()}`;
+  });
 }
