7ac52f2942d207967ff84689cdb50a52a0474263
Jared Kotoff <jkotoff@veritone.com>
2017-12-23 01:03:12 -0800

    Only clips from restricted channels will be shared by default

--- index.js
+++ index.js
@@ -14,89 +14,118 @@ const TWITCH_CHANNELS = generateChannelList(
   _.get(process, 'env.TWITCH_CHANNELS'),
 );
 const TWITCH_CLIENT_ID = _.get(process, 'env.TWITCH_CLIENT_ID') || null;
+const RESTRICT_CHANNELS = _.get(process, 'env.RESTRICT_CHANNELS') || true;
 const BROADCASTER_ONLY =
   _.get(process, 'env.BROADCASTER_ONLY') === 'true' || false;
 const MODS_ONLY = _.get(process, 'env.MODS_ONLY') === 'true' || false;
 const SUBS_ONLY = _.get(process, 'env.SUBS_ONLY') === 'true' || false;
 
-winston.log('info', 'Config settings:\n', {
-  DISCORD_WEBHOOK_URL,
-  TWITCH_CHANNELS,
-  BROADCASTER_ONLY,
-  MODS_ONLY,
-  SUBS_ONLY,
-});
-winston.log('info', `Twitch Client ID is ${TWITCH_CLIENT_ID ? '' : 'NOT '}set`);
+let TWITCH_CHANNEL_IDS = [];
+if (TWITCH_CLIENT_ID && RESTRICT_CHANNELS) {
+  resolveTwitchUsernamesToIds(TWITCH_CHANNELS).then(userIds => {
+    TWITCH_CHANNEL_IDS = userIds;
+    logStartInfo();
+  });
+}
 
-const tmiOptions = {
-  options: {
-    debug: _.get(process, 'env.LOG_LEVEL') === 'debug' || false,
-  },
-  connection: {
-    reconnect: true,
-  },
-  channels: TWITCH_CHANNELS,
-};
+function logStartInfo() {
+  winston.log('info', 'Config settings:\n', {
+    DISCORD_WEBHOOK_URL,
+    TWITCH_CHANNELS,
+    TWITCH_CHANNEL_IDS,
+    RESTRICT_CHANNELS,
+    BROADCASTER_ONLY,
+    MODS_ONLY,
+    SUBS_ONLY,
+  });
+  winston.log(
+    'info',
+    `Twitch Client ID is ${TWITCH_CLIENT_ID ? '' : 'NOT '}set`,
+  );
 
-const client = new tmi.client(tmiOptions);
+  createTmiClient();
+}
 
-// Check messages that are posted in twitch chat
-client.on('message', (channel, userstate, message, self) => {
-  winston.log('debug', 'New message');
-  winston.log('debug', 'Channel: ', channel);
-  winston.log('debug', 'Userstate: ', userstate);
-  winston.log('debug', 'Message: ', message);
+function createTmiClient() {
+  const tmiOptions = {
+    options: {
+      debug: _.get(process, 'env.LOG_LEVEL') === 'debug' || false,
+    },
+    connection: {
+      reconnect: true,
+    },
+    channels: TWITCH_CHANNELS,
+  };
 
-  // Don't listen to my own messages..
-  if (self) return;
-  // Broadcaster only mode
-  const isBroadcaster = userstate['badges'].broadcaster === '1';
-  if (BROADCASTER_ONLY && !isBroadcaster) {
-    winston.log('debug', `NON-BROADCASTER posted a clip: ${message}`);
-    return;
-  }
-  // Mods only mode
-  if (MODS_ONLY && !(userstate['mod'] || isBroadcaster)) {
-    winston.log('debug', `NON-MOD posted a clip: ${message}`);
-    return;
-  }
-  // Subs only mode
-  if (SUBS_ONLY && !userstate['subscriber']) {
-    winston.log('debug', `NON-SUB posted a clip: ${message}`);
-    return;
-  }
+  const client = new tmi.client(tmiOptions);
 
-  // Handle different message types..
-  switch (userstate['message-type']) {
-    case 'action':
-      // This is an action message..
-      break;
-    case 'chat':
-      if (message.indexOf('clips.twitch.tv/') !== -1) {
-        winston.log('debug', `Clip detected in message: ${message}`);
-        if (TWITCH_CLIENT_ID) {
-          postUsingTwitchAPI(message);
-        } else {
-          postUsingMessageInfo(message);
+  // Check messages that are posted in twitch chat
+  client.on('message', (channel, userstate, message, self) => {
+    winston.log('debug', 'New message');
+    winston.log('debug', 'Channel: ', channel);
+    winston.log('debug', 'Userstate: ', userstate);
+    winston.log('debug', 'Message: ', message);
+
+    // Don't listen to my own messages..
+    if (self) return;
+    // Broadcaster only mode
+    const isBroadcaster = userstate['badges'].broadcaster === '1';
+    if (BROADCASTER_ONLY && !isBroadcaster) {
+      winston.log('debug', `NON-BROADCASTER posted a clip: ${message}`);
+      return;
+    }
+    // Mods only mode
+    if (MODS_ONLY && !(userstate['mod'] || isBroadcaster)) {
+      winston.log('debug', `NON-MOD posted a clip: ${message}`);
+      return;
+    }
+    // Subs only mode
+    if (SUBS_ONLY && !userstate['subscriber']) {
+      winston.log('debug', `NON-SUB posted a clip: ${message}`);
+      return;
+    }
+
+    // Handle different message types..
+    switch (userstate['message-type']) {
+      case 'action':
+        // This is an action message..
+        break;
+      case 'chat':
+        if (message.indexOf('clips.twitch.tv/') !== -1) {
+          winston.log('debug', `Clip detected in message: ${message}`);
+          if (TWITCH_CLIENT_ID) {
+            postUsingTwitchAPI(message);
+          } else {
+            postUsingMessageInfo(message);
+          }
         }
-      }
-      break;
-    case 'whisper':
-      // This is a whisper..
-      break;
-    default:
-      // Something else ?
-      break;
-  }
-});
+        break;
+      case 'whisper':
+        // This is a whisper..
+        break;
+      default:
+        // Something else ?
+        break;
+    }
+  });
 
-// Connect the client to the server..
-client.connect();
+  // Connect the client to the server..
+  client.connect();
+}
 
 function postUsingTwitchAPI(message) {
   const clipId = getUrlSlug(message);
   twitchApiGetCall('clips', clipId).then(clipInfo => {
     winston.log('debug', 'Twitch clip results:', clipInfo);
+
+    if (
+      RESTRICT_CHANNELS &&
+      TWITCH_CHANNEL_IDS.indexOf(clipInfo.broadcaster_id) === -1
+    ) {
+      winston.log('info', 'Clip from unselected channel was posted in chat');
+      return;
+    }
+
     Promise.all([
       twitchApiGetCall('users', clipInfo.creator_id),
       twitchApiGetCall('games', clipInfo.game_id),
@@ -158,6 +187,33 @@ async function twitchApiGetCall(endpoint, id) {
   }
 }
 
+async function resolveTwitchUsernamesToIds(usernames) {
+  if (!TWITCH_CLIENT_ID) return [];
+
+  const usernameFuncs = usernames.map(async username => {
+    const options = {
+      uri: `https://api.twitch.tv/helix/users`,
+      qs: {
+        login: username.replace('#', ''),
+      },
+      headers: {
+        'Client-ID': TWITCH_CLIENT_ID,
+      },
+      json: true,
+    };
+    winston.log('info', `Calling /users?login=${username}`);
+    try {
+      const response = await request(options);
+      request(options);
+      return response.data[0].id;
+    } catch (err) {
+      winston.log('error', `Error calling twitch API /users:`, err);
+      return;
+    }
+  });
+  return await Promise.all(usernameFuncs).then(userIds => userIds);
+}
+
 function postToDiscord(val) {
   request.post(
     DISCORD_WEBHOOK_URL,
@@ -183,6 +239,6 @@ function generateChannelList(channelsString) {
   let channelArray = _.split(channelsString, ' ');
 
   return channelArray.map(channel => {
-    return `#${channel}`;
+    return `#${channel.toLowerCase()}`;
   });
 }
